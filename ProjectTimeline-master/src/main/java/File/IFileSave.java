4db51d49e3575a0b37c845a3115a777d896129d7
katyabarkalova <barkalovakatya@mail.ru>
2022-06-14 11:25:52 +0300

    1
    Изменила

--- ProjectTimeline-master/src/main/java/File/IFileSave.java
+++
@@ -1,9 +0,0 @@
-package File;
-
-import ProjectTimeLine.TimeLine.ProjectTimeLine;
-
-/** Интерфейс для сохранения проекта */
-public interface IFileSave {
-
-    public void save (ProjectTimeLine projectTimeLine);
-}
